3dd7483417dcecdcd50b8b5f102379d066db0bff
Alfonso <alfcrisci@gmail.com>
2020-04-21 12:34:23 +0200

    Update data_text.js

--- data/data_text.js
+++ data/data_text.js
@@ -1,12 +1,3 @@
 var linkTitle='';
 var titlecustom='Mappe provinciali COVID';
-var htmlcustom='<h2>Dati Covid</h2>
-<p>Tutti i giorni dalla repository COVID-19 Italia - Monitoraggio situazione 
-del Dipartimento della protezione civile italiana vengono distribuiti i dati relativi agli 
-<a href='https://it.wikipedia.org/wiki/SARS-CoV-2'>SARS-CoV-2</a>, 
-con licenza CC-BY-4.0. I cruscotti di dettagli sono raggiungibili agli indirizzi 
-<a href='http://arcg.is/C1unv'>DESKTOP</a> e <a href='http://arcg.is/081a51'>MOBILE</a></p>
-<p>fonte wikipedia</p>
-<p>Le mappe qui presentate sono una versione semplificata a livello di provincia con i dati normalizzati 
-( layer INCIDENZA ) a quelli di popolazione ISTAT e il tasso di variazione giornaliero.</p>
-<p>Alfonso Crisci - Istituto per la BioEconomia</p>';
+var htmlcustom='Dati Covid';
