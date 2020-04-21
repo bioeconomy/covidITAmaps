50f26ba97366e4f4a38d4c4f2dee33d582d09ec9
Alfonso <alfcrisci@gmail.com>
2020-04-21 12:27:50 +0200

    Update data_text.js

--- data/data_text.js
+++ data/data_text.js
@@ -1,8 +1,12 @@
 var linkTitle='';
 var titlecustom='Mappe provinciali COVID';
 var htmlcustom='<h2>Dati Covid</h2>
-<p style="font-size: 1.5em;">Tutti i giorni dalla repository '<span class="text-gray-dark mr-2">COVID-19 Italia - Monitoraggio situazione </span>' del Dipartimento della&nbsp; protezione civile italiana vengono distribuiti i dati relativi all'epidemia legata al <a title="SARS-CoV-2" href="https://it.wikipedia.org/wiki/SARS-CoV-2">SARS-CoV-2</a>, con licenza CC-BY-4.0. I cruscotti di dettagli sono raggiungibili agli indirizzi <a href="http://arcg.is/C1unv" rel="nofollow">DESKTOP</a> e <a href="http://arcg.is/081a51" rel="nofollow">MOBILE</a></p>
-<h2><img src="https://upload.wikimedia.org/wikipedia/commons/c/cf/SARS_virion.gif" alt="virus" width="300" height="200" /></h2>
+<p>Tutti i giorni dalla repository COVID-19 Italia - Monitoraggio situazione 
+del Dipartimento della protezione civile italiana vengono distribuiti i dati relativi agli 
+<a href='https://it.wikipedia.org/wiki/SARS-CoV-2'>SARS-CoV-2</a>, 
+con licenza CC-BY-4.0. I cruscotti di dettagli sono raggiungibili agli indirizzi 
+<a href='http://arcg.is/C1unv'>DESKTOP</a> e <a href='http://arcg.is/081a51'>MOBILE</a></p>
 <p>fonte wikipedia</p>
-<p style="font-size: 1.5em;">Le mappe qui presentate sono una versione semplificata a livello di provincia con i dati normalizzati ( layer INCIDENZA%) a quelli di popolazione ISTAT e il tasso di variazione giornaliero.</p>
-<p style="font-size: 1.5em;">Alfonso Crisci - Istituto per la BioEconomia</p>';
+<p>Le mappe qui presentate sono una versione semplificata a livello di provincia con i dati normalizzati 
+( layer INCIDENZA ) a quelli di popolazione ISTAT e il tasso di variazione giornaliero.</p>
+<p>Alfonso Crisci - Istituto per la BioEconomia</p>';
